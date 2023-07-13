f24d2804f02affc3e07e9b5b47985a2684cea635
hishmatrai <hishmatrai.2018@gmail.com>
2023-07-13 18:40:35 +0500

    update

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,60 +1,49 @@
 import React, { useState } from 'react';
 import { Navbar, Card } from '../../components';
+import './index.css'
 const Home = () => {
-    const [username, setUsername] = useState("");
+    const [userName, setUserName] = useState("");
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
-    const [messageType, setMessageType] = useState("");
-    const [isError, setIsError] = useState(true)
-    // email
-    const emaiAddress = (e) => {
+    const [isError, setError] = useState(true);
+    const [users, setUsers] = useState([]);
+    console.log("users", users)
+    const addEmailAddress = (e) => {
         setEmail(e.target.value)
     }
-    const addUserName = () => {
-        if (username === "") {
-            setMessage("user name required");
-            setMessageType("error");
-            setIsError(true)
+    const AddData = () => {
+        if (userName === "") {
+            setMessage("Username required !");
+            setError(true)
         } else if (email === "") {
-            setMessage("email address required");
-            setMessageType("error")
-            setIsError(true)
+            setMessage("Email required !");
+            setError(true)
         } else {
-            let user = {
-                username: username,
-                email: email
+            const user = {
+                userName: userName,
+                email: email,
+                edit: false
             }
-            setMessageType("success")
-            setIsError(false)
+            let newUser = [];
+            newUser.push(user)
+            setUsers([...users, ...newUser])
+            setError(false)
             setMessage("Success")
-            console.log("user", user)
+            setUserName("");
+            setEmail("")
+            console.log("user", user);
+            setTimeout(() => {
+                setMessage("")
+            }, 2000);
         }
     }
 
-
-    const users = [
-        {
-            name: "user 1",
-            email: "user1@email.com",
-            mobile: ["1231321", "4554545"],
-            status: "Pending",
-            userNameShow: true
-        },
-        {
-            name: "user 2",
-            email: "user2@email.com",
-            mobile: ["1231321"],
-            status: "Approved",
-            userNameShow: true
-        },
-        {
-            name: "user 3",
-            email: "user3@email.com",
-            mobile: [],
-            status: "Reject",
-            userNameShow: false
-        }
-    ]
+    // Update
+    const UpdateHandler = (index)=>{
+        console.log(users[index])
+        users[index].edit = false
+        setUsers([...users])
+    }
     return (
         <div>
             {/* <Navbar />
@@ -78,7 +67,7 @@ const Home = () => {
             {/* <hr /> */}
             {/* <h1>{name}</h1> */}
             {/* <button onClick={() => setName("iHunar")}>Update</button> */}
-            <p >{username}</p>
+            {/* <p >{username}</p>
             <hr />
             <input type='text' placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
             <input type='email' placeholder='Email address' value={email} onChange={(e) => emaiAddress(e)} />
@@ -90,7 +79,6 @@ const Home = () => {
             {users.map((v, i) => {
                 return (
                     <div key={i} style={{ border: "1px solid red", marginTop: "10px" }}>
-                        {/* <h1 style={{ display: v.userNameShow ? "block" : "none" }}>{v.name}</h1> */}
                       
                       {v.userNameShow &&  <h1 >{v.name}</h1> }
                        
@@ -100,12 +88,47 @@ const Home = () => {
                                 <p key={index}>{value}</p>
                             )
                         })}
-                        {/* reject red approve green pending yellow */}
                         <p style={{ backgroundColor: v.status === "Pending" ? "yellow" : v.status === "Reject" ? "red" : "green" }}>{v.status}</p>
                         <button>Show</button>
                     </div>
                 )
-            })}
+            })} */}
+            <input type='text' placeholder='Username' value={userName} onChange={(e) => setUserName(e.target.value)} />
+            <input type='email' name='email' placeholder='Email Address' value={email} onChange={(e) => addEmailAddress(e)} />
+            {message !== "" && <p style={{ color: isError ? "red" : "green" }}>{message}</p>}
+
+            <button onClick={AddData}>add</button>
+            <hr />
+            {users.length === 0 ?
+
+                <p>No data found</p> :
+                <div>
+                    {users.map((v, i) => {
+                        return (
+                            <div key={i} className='card'>
+                                {v.edit ?
+                                    <div>
+                                        <input type='text' placeholder='Username' value={v.userName} onChange={(e) => setUserName(e.target.value)} />
+                                        <input type='email' name='email' placeholder='Email Address' value={v.email} onChange={(e) => addEmailAddress(e)} />
+                                        <button onClick={()=> UpdateHandler(i)}>Update</button>
+                                    </div> :
+                                    <div>
+                                        <p>Username :- {v.userName}</p>
+                                        <p>Email :- {v.email}</p>
+                                        <button onClick={() => {
+                                            users[i].edit = true
+                                            setUsers([...users])
+
+                                        }}>Edit</button>
+                                        <button>Delete</button>
+                                    </div>
+                                }
+
+                            </div>
+                        )
+                    })}
+                </div>
+            }
         </div>
     )
 }
